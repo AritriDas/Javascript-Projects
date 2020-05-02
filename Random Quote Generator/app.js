137ebda95e143f2e0329249379c3c53210f7817b
AritriDas <dasaritri07@gmail.com>
2020-05-02 20:24:00 +0530

    Random Quote Generator 7

--- Random Quote Generator/app.js
+++ Random Quote Generator/app.js
@@ -1,53 +1,54 @@
-const quotes = {
+const quotes = [
     {
-        name : "Walt Disney",
-        quote : “The Way Get Started Is To Quit Talking And Begin Doing.”
+        name : 'Walt Disney',
+        quote : 'The Way Get Started Is To Quit Talking And Begin Doing.'
     },
     {
-        name : "Winston Churchill",
-        quote : “The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.” 
+        name : 'Winston Churchill',
+        quote : 'The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.'
     },
     {
-        name : "Will Rogers",
-        quote : “Don’t Let Yesterday Take Up Too Much Of Today.”
+        name : 'Will Rogers',
+        quote : 'Don’t Let Yesterday Take Up Too Much Of Today.'
     },
     {
-        name : "Vince Lombardi",
-        quote : “It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.”
+        name : 'Vince Lombardi',
+        quote : 'It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.'
     },
     {
-        name : "Steve Jobs",
-        quote : “If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.”
+        name : 'Steve Jobs',
+        quote : 'If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.'
     },{
-        name : "Rob Siltanen",
-        quote : “People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.”
+        name : 'Rob Siltanen',
+        quote : 'People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.'
     },
     {
-        name : "Maya Angelou",
-        quote : “We May Encounter Many Defeats But We Must Not Be Defeated.”
+        name : 'Maya Angelou',
+        quote : 'We May Encounter Many Defeats But We Must Not Be Defeated.'
     },
     {
-        name : "Henry Link",
-        quote : “We Generate Fears While We Sit. We Overcome Them By Action.”
+        name : 'Henry Link',
+        quote : 'We Generate Fears While We Sit. We Overcome Them By Action.'
     },
     {
-        name : "Hasidic Proverb",
-        quote : “The Man Who Has Confidence In Himself Gains The Confidence Of Others.”
+        name : 'Hasidic Proverb',
+        quote : 'The Man Who Has Confidence In Himself Gains The Confidence Of Others.'
     },
     {
-        name : "Albert Einstein",
-        quote : “Creativity Is Intelligence Having Fun.”
+        name : 'Albert Einstein',
+        quote : 'Creativity Is Intelligence Having Fun.'
     }
-}
+]
 
 const quoteBtn = document.querySelector('#quoteBtn');
 const quoteAuthor = document.querySelector('#quoteAuthor');
-const quote = document.querySelector('query');
+const quote = document.querySelector('#quote');
 
 quoteBtn.addEventListener('click', displayQuote);
 
 function displayQuote(){
 
     let num = Math.floor(Math.random()*quotes.length);
-
+    quoteAuthor.innerHTML = quotes[num].name;
+    quote.innerHTML = quotes[num].quote;
 }
