7367120e37df0a2b41844fbaedfc5e9ece622311
AritriDas <dasaritri07@gmail.com>
2020-05-06 13:02:40 +0530

    Change 5

--- Counter/app.js
+++ Counter/app.js
@@ -1,3 +1,18 @@
 let counter = document.querySelector('.counter');
 let lowerCount = document.querySelector('#lowerCount');
 let upperCount = document.querySelector('#upperCount');
+
+let count = 0;
+
+upperCount.addEventListener('click', incrementCounter);
+lowerCount.addEventListener('click', decrementCounter);
+
+function incrementCounter() {
+    count ++;
+    counter.innerHTML = count;
+}
+
+function decrementCounter() {
+    count --;
+    counter.innerHTML = count;
+}
