61050122585779c8154e07ac61e4e07e04b3ace7
AritriDas <dasaritri07@gmail.com>
2020-05-13 11:26:50 +0530

    Change 6

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,4 +1,19 @@
 const btns = document.querySelectorAll('.btn');
 const screen =  document.querySelector('.screen');
 const equal = document.querySelector('.btn-equal');
-const clear = document.querySelector('.btn-clear')
+const clear = document.querySelector('.btn-clear');
+
+for(let i = 0 ; i < btns.length ; i++) {
+    btns[i].addEventListener('click', function (){
+        let num = btns[i].getAttribute('data-num');
+        screen.value += num;
+    }) 
+}
+
+equal.addEventListener('click', function() {
+    let value = eval(screen.value);
+})
+
+clear.addEventListener('click', function() {
+    screen.value = '';
+})
