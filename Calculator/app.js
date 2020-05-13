a4fc051367cfd1bd7a587a747b9199906d9461ff
AritriDas <dasaritri07@gmail.com>
2020-05-13 11:09:18 +0530

    Change 5

---
+++ Calculator/app.js
@@ -0,0 +1,4 @@
+const btns = document.querySelectorAll('.btn');
+const screen =  document.querySelector('.screen');
+const equal = document.querySelector('.btn-equal');
+const clear = document.querySelector('.btn-clear')
