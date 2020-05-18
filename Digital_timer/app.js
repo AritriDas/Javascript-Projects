1c8eaea04f558f65101e5568f660a8d5305400f1
AritriDas <dasaritri07@gmail.com>
2020-05-18 20:27:26 +0530

    Change 4=5

--- Digital_timer/app.js
+++ Digital_timer/app.js
@@ -7,4 +7,20 @@ function showTime() {
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
 
+    let formatHours = convertFormat(hours);
+    hours = changeHours(hours);
+
+    function convertFormat(hours) {
+        let format = 'AM';
+
+        if(hours >= 12)
+            format = 'PM';
+
+        return format;
+    }
+
+    function changeHours(hours) {
+        hours = hours % 12;
+        return hours;
+    }
 }
