2076bab2995e02c01374d24ba017d5d3eb7f998a
AritriDas <dasaritri07@gmail.com>
2020-05-18 20:19:22 +0530

    Change 4

---
+++ Digital_timer/app.js
@@ -0,0 +1,10 @@
+const timer = document.querySelector('timer');
+
+function showTime() {
+
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+}
